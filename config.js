37aecad217fb3e324804e1c27b27f60a3507889a
MarkusAndersons <markus.andersons@gmail.com>
2016-11-27 13:52:09 +1030

    Create congif file

---
+++ config.js
@@ -0,0 +1,7 @@
+
+var config = {};
+
+config.title = "Configuration Profile Generator";
+
+
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var config = require('../config');
 var router = express.Router();
 
 /* GET home page. */
