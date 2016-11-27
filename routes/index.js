372e1178890492c0e113fc75276ba7dda3795fce
MarkusAndersons <markus.andersons@gmail.com>
2016-11-27 14:23:22 +1030

    Hide compiled sass

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: config.title });
 });
 
 module.exports = router;
