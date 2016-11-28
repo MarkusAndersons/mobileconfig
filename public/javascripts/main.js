ecfa0c3ef9737310ee9b0bd48badfb61d744b98d
MarkusAndersons <markus.andersons@gmail.com>
2016-11-28 12:26:20 +1030

    Create Landing page and verify email address client side

---
+++ public/javascripts/main.js
@@ -0,0 +1,32 @@
+// Main Javascript file for mobileconfig generator
+
+function checkEmail() {
+    var email = {};
+    email.address = document.getElementById('email').value;
+    var atCount = 0;
+    for (var i = 0; i < email.address.length; i++) {
+        if(email.address[i] == '@') {
+            atCount++;
+            console.log(atCount);
+        }
+    }
+    if(email.address == '') {
+        document.getElementById('message').innerHTML = '<p>This needs a valid email address</p>';
+    }
+    else if(atCount != 1) {
+        document.getElementById('message').innerHTML = '<p>This needs a valid email address</p>';
+    }
+    else if(atCount == 1) {
+        // post email
+        var newReq = new XMLHttpRequest();             // make new HTTP request
+        newReq.onreadystatechange = function() {
+            if (newReq.readyState==4 && newReq.status==200) {
+                console.log('Sent to server');
+                window.location = "http://localhost:3000/begin";
+            }
+        }
+        newReq.open("POST", "http://localhost:3000/valid_email", true);
+        newReq.setRequestHeader('Content-Type', 'application/json');
+        newReq.send(JSON.stringify(email));
+    }
+}
