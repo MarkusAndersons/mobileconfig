b12deec8d933b64d05df26fee3d8bac3603bf739
MarkusAndersons <markus.andersons@gmail.com>
2016-11-29 16:27:17 +1030

    start template generation

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var config = require('../config');
 var handlebars = require('handlebars');
+var fs = require('fs');
+var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
@@ -33,9 +35,51 @@ router.post('/valid_email', function(req, res) {
     res.sendStatus(200);
 });
 
+// display generate page
 router.get('/generate', function(req, res, next) {
     res.render('generate', { title: config.title });
 });
+
+// display settings options page
+router.get('/generate/certificate', function(req, res, next) {
+    res.render('generator/certificate', { title: config.title });
+});
+router.get('/generate/email', function(req, res, next) {
+    res.render('generator/email', { title: config.title });
+});
+router.get('/generate/wifi', function(req, res, next) {
+    res.render('generator/wifi', { title: config.title });
+});
+
+// get current settings for a profile
+router.post('/add_payload', function(req, res) {
+    console.log('recieved payload');
+    // determine type of payload
+    if (req.body.configType == "certificate") {
+        individualProfile("certificate_template", req);
+    } else if (req.body.configType == "email") {
+        individualProfile("email_template", req);
+    } else if (req.body.configType == "wifi") {
+        individualProfile("wifi_template", req);
+    } else if (req.body.configType == "restrictions") {
+        individualProfile("restrictions_template", req);
+    }
+
+    res.sendStatus(200);
+});
+
+
+// payload settings extraction
+function individualProfile(template, req) {
+    var inputSettings = req.body.configuration;
+    var template = handlebars.compile(fs.readFileSync(path.join(__dirname, "../config_templates", template + ".mobileconfig")), "utf-8");
+    var locSettings = template(inputSettings);
+
+    if (sess.configurations)
+        sess.configurations = sess.configurations.concat([locSettings]);
+    else
+        sess.configurations = [locSettings];
+}
 
 
 module.exports = router;
