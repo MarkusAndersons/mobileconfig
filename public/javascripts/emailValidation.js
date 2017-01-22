1950c1a54f8ad8c1fda927a8c5f06f723b07a753
MarkusAndersons <markus.andersons@gmail.com>
2017-01-22 14:13:50 +1030

    Email input complete

--- public/javascripts/emailValidation.js
+++ public/javascripts/emailValidation.js
@@ -1,3 +1,31 @@
+window.onload = function() {
+    $("#portNumberIn").val(143);
+    $("#portNumberOut").val(25);
+}
+
+function incomingSSLClicked() {
+    if ($("#portNumberIn").val() == 993)
+        $("#portNumberIn").val(143);
+    else
+        $("#portNumberIn").val(993);
+}
+function outgoingSSLClicked() {
+    if ($("#portNumberOut").val() == 465)
+        $("#portNumberOut").val(25);
+    else
+        $("#portNumberOut").val(465);
+}
+var samePasswords = false;
+function outgoingPasswordSameAsIncoming() {
+    if (samePasswords) {
+        $("#outgoingPasswordBlock").show();
+        samePasswords = false;
+    } else {
+        $("#outgoingPasswordBlock").hide();
+        samePasswords = true;
+    }
+}
+
 function submit() {
-    
+
 }
